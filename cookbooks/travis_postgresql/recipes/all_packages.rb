3dcdabf040dd72fc5e83d1362a2bc3da32ba017a
max.bender1 <max.bender1@ibm.com>
2018-10-03 16:37:48 -0400

    only this

--- cookbooks/travis_postgresql/recipes/all_packages.rb
+++ cookbooks/travis_postgresql/recipes/all_packages.rb
@@ -3,13 +3,14 @@ include_recipe 'travis_postgresql::pgdg'
 
 include_recipe 'travis_postgresql::client'
 
-package(
-  TravisPostgresqlMethods.pg_versions(node).map do |v|
-    %W[
-      postgresql-#{v}
-      #{node['travis_postgresql']['contrib_modules'] ? "postgresql-contrib-#{v}" : ''}
-    ]
-  end.flatten
-)
+# package(
+#   TravisPostgresqlMethods.pg_versions(node).map do |v|
+#     %W[
+#       postgresql-#{v}
+#       #{node['travis_postgresql']['contrib_modules'] ? "postgresql-contrib-#{v}" : ''}
+#     ]
+#   end.flatten
+# )
+package 'postgresql-9.5'
 
 include_recipe 'travis_postgresql::postgis' if node['travis_postgresql']['postgis_version']
