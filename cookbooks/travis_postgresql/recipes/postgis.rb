2a7658eb0a10caf9c74e987d3789345ff4843d93
max.bender1 <max.bender1@ibm.com>
2018-10-03 16:50:41 -0400

    updates

--- cookbooks/travis_postgresql/recipes/postgis.rb
+++ cookbooks/travis_postgresql/recipes/postgis.rb
@@ -1,10 +1,13 @@
 ppv = node['travis_postgresql']['postgis_version']
 
-package(
-  TravisPostgresqlMethods.pg_versions(node).map do |v|
-    %W[
-      postgresql-#{v}-postgis-#{ppv}
-      postgresql-#{v}-postgis-#{ppv}-scripts
-    ]
-  end.flatten
-)
+# package(
+#   TravisPostgresqlMethods.pg_versions(node).map do |v|
+#     %W[
+#       postgresql-#{v}-postgis-#{ppv}
+#       postgresql-#{v}-postgis-#{ppv}-scripts
+#     ]
+#   end.flatten
+# )
+
+package 'postgresql-9.5-postgis-2.2'
+package 'postgresql-9.5-postgis-scripts'
