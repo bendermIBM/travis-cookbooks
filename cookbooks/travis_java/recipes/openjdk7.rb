811f32167e4141932c438f8924a1a4a3bd0ee0e2
max.bender1 <max.bender1@ibm.com>
2019-01-30 16:27:35 -0500

    one more?

--- cookbooks/travis_java/recipes/openjdk7.rb
+++ cookbooks/travis_java/recipes/openjdk7.rb
@@ -1,12 +1,6 @@
 if node['kernel']['machine'] == 'ppc64le' && node['lsb']['codename'] == 'xenial'
   include_recipe 'travis_java::openjdk-r'
   package 'openjdk-7-jdk'
-<<<<<<< HEAD
-=======
-elsif node['kernel']['machine'] == 's390x' && node['lsb']['codename'] == 'xenial'
-  include_recipe 'travis_java::openjdk-r'
-  package 'openjdk-7-jdk'
->>>>>>> 40a442d911637cbe2147d3e99a2fb86734a2025d
 else
   package %w[
     icedtea-7-plugin
