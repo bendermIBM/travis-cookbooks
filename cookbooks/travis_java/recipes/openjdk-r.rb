41d353f3e5bb47e41b4f6566b803a147bd12a063
max.bender1 <max.bender1@ibm.com>
2019-01-30 19:57:46 -0500

    java update

--- cookbooks/travis_java/recipes/openjdk-r.rb
+++ cookbooks/travis_java/recipes/openjdk-r.rb
@@ -1,10 +1,17 @@
-apt_repository 'openjdk-r-java-ppa' do
-  uri 'http://ppa.launchpad.net/openjdk-r/ppa/ubuntu'
-  distribution node['lsb']['codename']
-  components %w[main]
-  key '86F44E2A'
-  keyserver 'hkp://ha.pool.sks-keyservers.net'
-  retries 2
-  retry_delay 30
-  action :add
+if node['kernel']['machine'] == 's390x'
+  apt_repository 'openjdk-r-ppa' do
+    uri 'ppa:openjdk-r'
+    distribution node['lsb']['codename']
+  end
+else
+  apt_repository 'openjdk-r-java-ppa' do
+    uri 'http://ppa.launchpad.net/openjdk-r/ppa/ubuntu'
+    distribution node['lsb']['codename']
+    components %w[main]
+    key '86F44E2A'
+    keyserver 'hkp://ha.pool.sks-keyservers.net'
+    retries 2
+    retry_delay 30
+    action :add
+  end
 end
--- cookbooks/travis_java/recipes/openjdk9.rb
+++ cookbooks/travis_java/recipes/openjdk9.rb
@@ -1,6 +1,7 @@
 if node['lsb']['codename'] == 'xenial' && node['kernel']['machine'] == 'ppc64le'
   package %w[icedtea-9-plugin openjdk-9-jdk]
 elsif node['lsb']['codename'] == 'xenial' && node['kernel']['machine'] == 's390x'
+  include_recipe 'travis_java::openjdk-r'
   package %w[icedtea-9-plugin openjdk-9-jdk]
 else
   include_recipe 'travis_java::openjdk-r'
